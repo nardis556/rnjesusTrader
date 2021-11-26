b2b1514bae7a1539107eee369bff13a209615f0d
nardis556 <82609175+nardis556@users.noreply.github.com>
2021-11-26 12:05:07 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 BINANCE_API_KEY = 'API_KEY'
 BINANCE_SECRET_KEY = 'SECRET_KEY'
 
-marketPair = 'ETH/USDT'
+marketPair = 'ETH/USDT' #edit quote & base manually on ranbot3.py
 #orderAmount = '0.02' \\ working on quote * base
 #leverage?
 #quote base replaced on main
